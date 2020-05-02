c663e9061f18894d109d657801c92c5986204016
Patrick Goeler <patrickvongoeler@gmail.com>
2020-05-02 23:24:41 +0200

    cleanup, first layout, icons

--- pages/index.vue
+++ pages/index.vue
@@ -1,74 +1,65 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        patrickgoeler.github.io
-      </h1>
-      <h2 class="subtitle">
-        My Personal Website
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="container">
+        <div>
+            <h1 class="text-6xl">
+                PGVR
+            </h1>
+            <h2 class="text-xl">
+                Patrick Goeler
+            </h2>
+            <div class="flex justify-center">
+                <a
+                    v-for="(social, index) in socialArray"
+                    :key="social.name"
+                    :href="social.url"
+                    :style="animDelay(index)"
+                    :tooltip="social.name"
+                    class="hover:text-green-500"
+                    target="_blank"
+                >
+                    <Icon :icon="social.icon" />
+                </a>
+            </div>
+            <div class="flex justify-center items-center">
+                <Icon icon="copyright" />
+                {{ footer() }}
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Logo from '~/components/Logo.vue'
+import Vue from "vue"
+import { Component } from "vue-property-decorator"
+import socials from "@/static/socials.json"
+import Icon from "@/components/Icon.vue"
 
-export default Vue.extend({
-  components: {
-    Logo
-  }
-})
+@Component({ name: "Index", components: { Icon } })
+export default class Index extends Vue {
+    name = "Index"
+    socialArray = socials
+
+    animDelay(i: number) {
+        return `animation-delay: ${i * 50}ms`
+    }
+
+    iconClass(c: string) {
+        return `${c} index-contact-link-icon`
+    }
+
+    footer() {
+        return `Patrick Goeler ${new Date().getFullYear()}`
+    }
+}
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
 .container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
 }
 </style>
